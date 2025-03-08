ffd81f42a8961fec30782f07533c951ad7584db4
sgarcia97 <83793406+sgarcia97@users.noreply.github.com>
2025-03-07 17:05:12 -0700

    Updates

--- app/index.tsx
+++ app/index.tsx
@@ -5,15 +5,20 @@ import getUsers from "../lib/supabase_crud"
 
 export default function App() {
   const [data, setData] = useState<any>(getUsers) 
-  console.log(data) 
+  
   return (
     <View style={styles.container}>
-      <Text>Users</Text>
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={styles.title}>Users</Text>
+      <View style={styles.recordWrapper}>
       <FlatList
-        data={data}
+        data={data._j}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (<Text style={{color:"#000"}} key={item.id}>{item.username}</Text>)}
+        renderItem={({ item }) => (
+        <View key={item.id} style={styles.record}>
+            <Text style={{color:"#000"}}>Username: {item.username}</Text>
+            <Text style={{color:"#000"}}>Password: {item.password}</Text>
+            </View>
+        )}
       />
     </View>
       <StatusBar style="auto" />
@@ -23,9 +28,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    padding:10,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
+  recordWrapper:{
+    display:'flex',
+    gap:10,
+  },
+  title:{
+    fontWeight:800,
+    fontSize:30,
+    marginBottom:10
+  },
+  record: {
+    padding:10,
+    borderWidth:1,
+    borderColor:"#eaeaea",
+    marginBottom:10,
+    borderRadius:5
+  }
 });
